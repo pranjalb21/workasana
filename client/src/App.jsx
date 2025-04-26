14d48cefeea45f6d5715bea98aa8b37a35208f62
Pranjal Gain <pranjalgain052@gmail.com>
2025-04-27 00:37:46 +0530

    Initialize server with package.json and essential dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function App() {
+    return (
+        <>
+            <h1 className="fs-5">Hi</h1>
+        </>
+    );
+}
+
+export default App;
