b06448c00c2b912f7972c5eeda4e31d2e58f0bf3
Pranjal Gain <pranjalgain052@gmail.com>
2025-05-13 04:03:49 +0530

    fix: uncomment SignUpPage route in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,8 @@ import LoginPage from "./pages/LoginPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import ProjectDetailsPage from "./pages/ProjectDetailsPage";
 import TaskDetailsPage from "./pages/TaskDetailsPage";
-// import SignUpPage from "./pages/SignUpPage";
+import SignUpPage from "./pages/SignUpPage";
+
 const isAuthenticated = false; // Replace this with actual authentication logic
 
 function App() {
@@ -22,8 +23,8 @@ function App() {
         <>
             {loading && <Loader />}
             <Routes>
+                <Route path="/signup" element={<SignUpPage />} />
                 <Route path="/login" element={<LoginPage />} />
-                {/* <Route path="/signup" element={<SignUpPage />} /> */}
 
                 {/* Protected Routes */}
                 <Route
