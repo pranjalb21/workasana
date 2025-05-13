84134b99d059ceb6e1b61067faf69e3ae11cbd8e
Pranjal Gain <pranjalgain052@gmail.com>
2025-05-13 08:20:29 +0530

    fix: update import path for DataProvider to use correct casing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { DataProvider } from "../contexts/application.context.jsx";
 import { ToastContainer, Zoom } from "react-toastify";
+import { DataProvider } from "./contexts/Application.context.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
