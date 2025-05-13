ee5bec53aae330af50e2e9687b2efa428e3debe4
Pranjal Gain <pranjalgain052@gmail.com>
2025-05-13 08:21:35 +0530

    fix: update import path for DataProvider to use correct relative path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer, Zoom } from "react-toastify";
-import { DataProvider } from "./contexts/Application.context.jsx";
+import { DataProvider } from "../contexts/Application.context.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
